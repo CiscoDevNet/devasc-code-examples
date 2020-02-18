a174f5c1d59516cf73cae45b252a10169fc80759
Adrienne Moherek <amoherek@cisco.com>
2020-02-18 16:22:55 -0600

    change back to not render template

--- sample-app/sample-app.py
+++ sample-app/sample-app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask import request
-from flask import render_template
 
 sample = Flask(__name__)
 
 @sample.route("/")
 def main():
-    return render_template("index.html")
+    return "You are calling me from "+ request.remote_addr
 
 if __name__ == "__main__":
     sample.run(port=8080, host="0.0.0.0")
