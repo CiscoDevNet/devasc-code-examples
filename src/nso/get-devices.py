9d1f965699f985e3d991802b6da04d339e505148
Anne Gentle <agentle@cisco.com>
2020-02-21 18:30:16 -0600

    Up to NSO

--- src/nso/get-devices.py
+++ src/nso/get-devices.py
@@ -1,2 +1,22 @@
 # Add code from the NSO hands-on lab here
 
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+username = 'developer'
+password = 'BallGreenFoot23!'
+
+url = "https://devasc-nso-1.cisco.com/restconf/data/tailf-ncs:devices/device=ios0/config"
+
+headers = {
+    'Content-Type': "application/yang-data+json"
+    }
+
+# Supress credential warning for this exercise
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+response = requests.get(url,
+                        auth=(username, password),
+                        headers=headers,
+                        verify=False)
+
+print(response.text)
